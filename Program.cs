079f4b185353459f38e432e26a28593e320b5853
Ibrahim Fatade <ibfatade18@gmail.com>
2026-02-02 15:47:02 +0000

    Working with int and long

--- Program.cs
+++ Program.cs
@@ -2,3 +2,10 @@
 Console.WriteLine("Hello, World!");
 int b = 9;
 Console.WriteLine(b);
+
+int number1 = 2100000000;
+int number2 = 2100000000;
+
+long total = (long)number1 + (long)number2;
+Console.WriteLine(total);
+
